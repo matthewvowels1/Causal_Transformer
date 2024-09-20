2829f76d36cf057efe966a61301d5431e6f90d52
mathieu rochat <mathieu.louis.rochat@gmail.com>
2024-09-20 15:32:07 +0200

    Switch to instantiate_CaT

--- Eval/IHDP/eval.py
+++ Eval/IHDP/eval.py
@@ -2,22 +2,18 @@ import networkx as nx
 import numpy
 import numpy as np
 
-from Eval.eval_utils import train_model
+from Eval.eval_utils import train_model, instantiate_CaT
 from CaT.datasets import reorder_dag, get_full_ordering
 from utils.inference import CausalInference
-from CaT.test_model import CaT
 from loader import load_IHDP
 import torch
 
 
-def instantiate_model(device):
-    dropout_rate = 0.0
-    ff_n_embed = 4
-    num_heads = 3
-    n_layers = 1
-    embed_dim = 5
-    head_size = 4
-    input_dim = 1
+if __name__ == "__main__":
+    seed = 4
+    np.random.seed(seed=seed)
+    torch.manual_seed(seed)
+    device = 'cuda'
 
     DAGnx = nx.DiGraph()
 
@@ -34,30 +30,10 @@ def instantiate_model(device):
     DAGnx = reorder_dag(dag=DAGnx)  # topologically sorted dag
     causal_ordering = get_full_ordering(DAGnx)
     print(causal_ordering)
-    model = CaT(
-        input_dim=input_dim,
-        embed_dim=embed_dim,
-        dropout_rate=dropout_rate,
-        head_size=head_size,
-        num_heads=num_heads,
-        ff_n_embed=ff_n_embed,
-        dag=DAGnx,
-        causal_ordering=causal_ordering,
-        n_layers=n_layers,
-        device=device,
-        var_types=var_types,
-        activation_function='Swish'
-    ).to(device)
-    return model
 
 
-if __name__ == "__main__":
-    seed = 1
-    np.random.seed(seed=seed)
-    torch.manual_seed(seed)
-    device = 'cuda'
     for i in range(1, 101):
-        model = instantiate_model(device=device)
+        model = instantiate_CaT(device=device, var_types=var_types, DAGnx= DAGnx, embed_dim=20, n_layers=2)
         dataset = {}
         true_ite = {}
         for split in ('train', 'test'):
@@ -67,7 +43,7 @@ if __name__ == "__main__":
             dataset[split] = data
             true_ite[split] = y1 - y0
 
-        train_model(model=model, train=dataset['train'], test=dataset['test'], device=device)
+        train_model(model=model, train=dataset['train'], test=dataset['test'], device=device, max_iters=5000)
 
         for split in ('train', 'test'):
             ci = CausalInference(model=model, device=device)
@@ -85,5 +61,5 @@ if __name__ == "__main__":
                 np.mean((true_ite[split].squeeze() - est_ite) * (true_ite[split].squeeze() - est_ite)))
             eate = np.abs(true_ite[split].mean() - est_ate)
 
-            with open("output.txt", "w") as file:
+            with open("output.txt", "a") as file:
                 file.write(f"i: {i}\nsplit: {split}\npehe: {pehe}\neate: {eate}\n")
