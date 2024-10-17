8035d5b9a95a953f00d3485a43c21d71208a81ab
mathieu rochat <mathieu.louis.rochat@gmail.com>
2024-10-17 11:44:57 +0200

    x->t

--- Eval/IHDP/loader.py
+++ Eval/IHDP/loader.py
@@ -6,10 +6,10 @@ def load_IHDP(replication=1, split='train'):
     path = os.path.join(os.path.dirname(__file__), f"data/ihdp_npci_{split}_{replication}.csv")
     data = np.loadtxt(path, delimiter=',', skiprows=1)
 
-    x, y = data[:, 0][:, np.newaxis], data[:, 1][:, np.newaxis]
+    t, y = data[:, 0][:, np.newaxis], data[:, 1][:, np.newaxis]
     y0, y1, z = data[:, 3][:, np.newaxis], data[:, 4][:, np.newaxis], data[:, 5:]
     # custom fix
     z[:, 13] -= 1
 
-    return z, x, y, y1, y0
+    return z, t, y, y1, y0
 
